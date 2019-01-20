63fe10982c997588df5189a196fedc6220e73ba7
vlasovt <tarasvlasov@gmail.com>
2019-01-20 18:24:41 -0500

    added newer training model and added text to About screen

--- NewsMapClient/src/views/About.vue
+++ NewsMapClient/src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <h1>This is an about page</h1>
+    <h1>This project is part of an effort to vusualize the stream of daily news on the world map while also classifying them as properly as it is possible by using the classification algorythms (machine learning)</h1>
   </div>
 </template>
